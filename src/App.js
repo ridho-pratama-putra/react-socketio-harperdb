2a833134c0551db40694b304942bd612e1dbcf15
ridho <ridho.putra@btpn.com>
2023-01-09 15:22:38 +0700

    initialize socket & create room and username state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useState } from 'react'; // Add this
 import Home from './pages/home';
+import io from 'socket.io-client'; // Add this
+
+const socket = io.connect('http://localhost:4000'); // Add this -- our server will run on port 4000, so we connect to it from here
 
 function App() {
+    const [username, setUsername] = useState(''); // Add this
+    const [room, setRoom] = useState(''); // Add this
+
     return (
         <Router>
             <div className='App'>
                 <Routes>
-                    <Route path='/' element={<Home/>}/>
+                    <Route path='/' element={
+                        <Home
+                            username={username} // Add this
+                            setUsername={setUsername} // Add this
+                            room={room} // Add this
+                            setRoom={setRoom} // Add this
+                            socket={socket} // Add this
+                        />
+                    }/>
                 </Routes>
             </div>
         </Router>
